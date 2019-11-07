347beb1b3c7291bdc1ff0104f1bb22cef5bc76bd
praba <48834999+Prabakar19@users.noreply.github.com>
2019-11-08 00:24:09 +0530

    Initial

---
+++ SeleniumTest.py
@@ -0,0 +1,19 @@
+#first selenium code in python
+#recognize food type using caloriemama.ai
+
+from selenium import webdriver
+import time
+browser = webdriver.Firefox() #my browser is firefox there's different things for other browsers
+browser.get('https://www.caloriemama.ai/api')
+
+upload = browser.find_element_by_class_name('file-upload')
+upload.send_keys('/media/pizza.jpeg') #enter your image file location here
+
+time.sleep(5) #i add this because when i write this code i had poor network
+get = browser.find_elements_by_class_name('group-name')
+
+for got in get:
+	print(got.text,"\n") #first result is  high accuracy one
+
+#if need only one result, remove for loop and change 'find_elements_by_class_name' to 'find_element_by_class_name' 
+#print get.text
